f5e03f5e2402dccba839e97906233683085127a6
lepreto <pretorius1724@gmail.com>
2022-06-02 07:05:19 -0700

    betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - using printf
+ *Return: we returing 0 again
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
