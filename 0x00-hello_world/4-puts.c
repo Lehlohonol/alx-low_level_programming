b0d46f28f23abcd4f11cbba84d9c8b2a6f0482fe
lepreto <pretorius1724@gmail.com>
2022-06-02 06:28:47 -0700

    lets check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-
 /**
- * C first file project
- * 
- * Alx 
- * Finising this program
+ *<stdio.h> - Input Output
+ *
+ *void - we check code
+ *
  */
+
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");		return (0);
+{;
+	puts("\"Programming is like building a multilingual puzzle");
+		return(0);
 }
