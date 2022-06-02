c9d1aed03302dabd27561bedc62e6905ff8228f0
lepreto <pretorius1724@gmail.com>
2022-06-02 05:31:08 -0700

    correction3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
+
+
 /**
  * C first file project
  * 
  * Alx 
  * Finising this program
  */
-
-#include <stdio.h>
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
