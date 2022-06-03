65e75a56ddd33abc9628f10f5782fbacbf2f4399
lepreto <pretorius1724@gmail.com>
2022-06-03 05:15:27 -0700

    lets check

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lower cap char
+ * new line
+ * Return: 0 with purchar
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+
+}
