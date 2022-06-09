ad29ee583422651b86864cc0e2207ec168c58c96
lepreto <pretorius1724@gmail.com>
2022-06-09 10:08:11 -0700

    correct one

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,21 @@
 
 
 /**
- * _isupper - Entry fuction
- * c: will  be the charector to check
+ * _isupper - Entry fn
+ * @c: will  be the charector to check
  * Return:  c uppercase or 0
  */
+
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
