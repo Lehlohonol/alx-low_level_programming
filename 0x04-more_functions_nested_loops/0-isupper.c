0630bed13ef2b5ab075907575967059765330426
lepreto <pretorius1724@gmail.com>
2022-06-09 10:00:19 -0700

    capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * _isupper - Entry fuction
+ * c: will  be the charector to check
+ * Return:  c uppercase or 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
