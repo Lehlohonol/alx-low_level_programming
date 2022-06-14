18b03541ad612667247a19793db72a35815b8b45
lepreto <pretorius1724@gmail.com>
2022-06-14 05:59:40 -0700

    First commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,29 +3,26 @@
 #include <stdio.h>
 
 /**
- * rev_string - reverses a string
+ * print_rev - prints the string in reverse
+ * folowed by a new line
  * @s:input
  * Return: Always 0
- */
-
-void rev_string(char *s)
+*/
+void print_rev(char *s)
 {
-	char *t = s;
-	char m[1000];
-	short c = 0;
+	int count = 0;
 
 	while (*s != '\0')
 	{
-		m[c] = *s;
 		s++;
-		c++;
+		count++;
 	}
-	c = 0;
 
-	while (s > t)
+	while (count > 0)
 	{
 		s--;
-		*s = m[c];
-		c++;
+		putchar(*s);
+		count--;
 	}
+	putchar('\n');
 }
