0b38ec71c684250a5dc14021cd0f0b9654258ecd
lepreto <pretorius1724@gmail.com>
2022-06-17 08:23:59 -0700

    checking

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,30 @@
 /**
- * _strncat - a function
+ * _strncat - a function that concatenates
  *
- * @dests: pointer to destination
+ * @dest: pointer to destinations
  * @src: pointer to source
- * @n: most number of bytes
+ * @n: most number of bytes from @src
  *
- * Return: @dest
+ * Return: @dests
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dests, char *src, int n)
 {
-	int c;
-	int i;
+	int c, j;
 
 	c = 0;
 
+	/*find size of dest array*/
 	while (dests[c])
 		c++;
 
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	 */
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dests[c + j] = src[j];
-
+	/*null terminate destinations*/
 	dests[c + j] = '\0';
 
 	return (dests);
