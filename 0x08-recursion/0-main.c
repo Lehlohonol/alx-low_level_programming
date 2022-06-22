e3d915bb6475944507c6c1a74a80b1d92d806a14
lepreto <pretorius1724@gmail.com>
2022-06-22 06:02:25 -0700

    puts

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * main - function with printing instruction
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+  _puts_recursion("Puts with recursion");
+  return (0);
+}
