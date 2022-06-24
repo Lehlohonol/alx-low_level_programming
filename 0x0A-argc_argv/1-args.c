31dbf75966e4be181d0ba6d80f3603e3d99cbb7c
lepreto <pretorius1724@gmail.com>
2022-06-24 02:36:47 -0700

    arg passed to program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; 
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
