41dfec1cebbe37afb3739f2b701e68b39d911a90
root <Hloni>
2023-07-24 21:14:25 -0700

    checking with checker

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - printing a string function on request
+ *
+ * @s: string passed to function
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
