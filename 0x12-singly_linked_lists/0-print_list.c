06f5139ba239c62f2cd48d26d78f3d2aee87941b
hloni <pretorius1724@gmail.com>
2023-07-31 05:22:36 -0700

    checking init commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * Prototype: print_list - function to print list
+ *
+ * @h: pointer to the beginning of the list
+ * 
+ * Return: The count of element
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
