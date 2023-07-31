4183e102ebd25e70e74d86bad4097f0569f8b620
hloni <pretorius1724@gmail.com>
2023-07-31 12:20:55 -0700

    free memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Prototype: free_grid - freeing matrix
+ *
+ * @grid: matrix being passed
+ * @height: freeing using this variable
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
+}
