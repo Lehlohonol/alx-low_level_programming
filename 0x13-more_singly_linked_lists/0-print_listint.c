443666de96756d083960e16faa6f823439b6f693
hloni <pretorius1724@gmail.com>
2023-08-01 03:43:38 -0700

    printed the number of content in a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
+#include "lists.h"
 
+/**
+ * Prototype: print_listint - function to print all elements of list
+ * 
+ * @h: pointer to the elements of lists
+ * 
+ * Return: count of the nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n); 
+		h = h->next; 
+		count++;
+	}
+	return (count);
+}
