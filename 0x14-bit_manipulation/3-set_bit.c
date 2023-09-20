92a59afb80c024c1e1a2fd31a16768eadd16a292
lepreto <pretorius1724@gmail.com>
2023-09-20 06:37:40 -0700

    betty fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	if ((sizeof(unsigned long int) * 8) < index)
 		return (-1);
-	bits= 1;
+	bits = 1;
 	bits = bits << index;
 	*n = *n | bits;
 	return (1);
