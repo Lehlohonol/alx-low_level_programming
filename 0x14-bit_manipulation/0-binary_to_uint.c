44357d86ece08e9ada0e424f391bdfa719efe3e7
Hloni <pretorius1724@gmail.com>
2023-11-23 10:28:31 -0800

    initialization files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * Fuction to convert binary to unsigned int -  binary_to_uint(const char *b);
+ * Binary number used :@b
+ * Return: 0 else run the conditions statements
+ */
+
+unsigned int binary_to_uint(const char *b);
+{
+
+}
